3aee06d74cd9c7077ca7fc4a2038ed236eb17cd9
kdeptula <contact@deptula.it>
2025-07-19 10:51:19 +0200

    skrypt transpozycja

--- docs/js/qrcode.js
+++ docs/js/qrcode.js
@@ -1,22 +1,13 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const h1 = document.querySelector("h1");
-      if (h1) {
-        const qrDiv = document.createElement("div");
-        qrDiv.id = "qr-code";
-        qrDiv.style.marginTop = "1em";
-        qrDiv.style.marginBottom = "2em";
-        h1.insertAdjacentElement("afterend", qrDiv);
+const qrDiv = document.createElement("div");
+document.querySelector("h1").insertAdjacentElement("afterend", qrDiv);
 
-        const script = document.createElement("script");
-        script.src = "https://cdn.jsdelivr.net/npm/qrcodejs/qrcode.min.js";
-        script.onload = () => {
-          new QRCode(qrDiv, {
-            text: window.location.href,
-            width: 128,
-            height: 128,
-          });
-        };
-        document.body.appendChild(script);
-      }
-});
-
+const script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/npm/qrcodejs/qrcode.min.js";
+script.onload = () => {
+  new QRCode(qrDiv, {
+    text: window.location.href,
+    width: 128,
+    height: 128,
+  });
+};
+document.body.appendChild(script);
